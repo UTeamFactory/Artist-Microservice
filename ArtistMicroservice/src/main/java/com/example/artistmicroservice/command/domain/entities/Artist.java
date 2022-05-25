69e12e36154f592fc04d1c4388f5d9dfc85c5ed0
Sebastian Gonzales <sgabad@hotmail.com>
2022-05-25 18:59:09 -0500

    Refactoring Artist

--- ArtistMicroservice/src/main/java/com/example/artistmicroservice/command/domain/entities/Artist.java
+++ ArtistMicroservice/src/main/java/com/example/artistmicroservice/command/domain/entities/Artist.java
@@ -1,22 +1,115 @@
 package com.example.artistmicroservice.command.domain.entities;
 
+import com.example.artistmicroservice.command.domain.values.*;
+import lombok.Data;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
+import javax.persistence.*;
 
+@Entity(name = "Artist")
+@Table(name = "artists")
+@Data
 @Aggregate
-public class Artist extends User {
+public class Artist {
+
     @AggregateIdentifier
-    private String firstName;
-    private String lastName;
-    private String alias;
-    private String description;
-    private String phrase;
-    private String image;
-    private String instagramLink;
-    private String facebookLink;
-    private String twitterLink;
+    @EmbeddedId
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "BINARY(16)"))
+    })
+    private UserId id;
 
-    protected Artist(){}
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "username", length = 30, nullable = false))
+    })
+    private Username username;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "password", length = 75, nullable = false))
+    })
+    private Password password;
+
+    @Embedded
+    private AuditTrail auditTrail;
+
+    @AggregateIdentifier
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "firstname", length = 75, nullable = false))
+    })
+    private Firstname firstname;
+
+
+    @AggregateIdentifier
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "lastname", length = 75, nullable = false))
+    })
+    private Lastname lastname;
+
+    @AggregateIdentifier
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "alias", length = 75, nullable = false))
+    })
+    private Alias alias;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "description", length = 200, nullable = false))
+    })
+    private Description description;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "phrase", length = 200, nullable = false))
+    })
+    private Phrase phrase;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "image", nullable = false))
+    })
+    private Image image;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "instagram_link", nullable = true))
+    })
+    private Link instagramLink;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "facebook_link", nullable = true))
+    })
+    private Link facebookLink;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "value", column = @Column(name = "twitter_link", nullable = true))
+    })
+    private Link twitterLink;
+
+    public Artist(UserId id, Username username, Password password, AuditTrail auditTrail, Firstname firstname, Lastname lastname, Alias alias, Description description, Phrase phrase, Image image, Link instagramLink, Link facebookLink, Link twitterLink) {
+        setId(id);
+        setUsername(username);
+        setPassword(password);
+        setFirstname(firstname);
+        setLastname(lastname);
+        setAuditTrail(auditTrail);
+        setAlias(alias);
+        setDescription(description);
+        setPhrase(phrase);
+        setImage(image);
+        setInstagramLink(instagramLink);
+        setFacebookLink(facebookLink);
+        setTwitterLink(twitterLink);
+    }
+
+    protected Artist(){
+
+    }
 
 }
