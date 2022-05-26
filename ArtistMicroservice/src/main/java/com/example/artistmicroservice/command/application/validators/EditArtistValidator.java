7415264e925ddaaa6e9cb42b14095278ee1d4e2c
George <sanchez28112001@gmail.com>
2022-05-25 23:13:01 -0500

    added validators package

--- ArtistMicroservice/src/main/java/com/example/artistmicroservice/command/application/validators/EditArtistValidator.java
+++ ArtistMicroservice/src/main/java/com/example/artistmicroservice/command/application/validators/EditArtistValidator.java
@@ -1,4 +1,73 @@
 package com.example.artistmicroservice.command.application.validators;
 
+import com.example.artistmicroservice.command.application.dtos.request.EditArtistRequest;
+import com.example.artistmicroservice.command.domain.entities.Artist;
+import com.example.artistmicroservice.command.infrastructure.ArtistRegistry;
+import com.example.artistmicroservice.command.infrastructure.ArtistRegistryRepository;
+import com.example.artistmicroservice.common.application.Notification;
+import org.axonframework.messaging.unitofwork.DefaultUnitOfWork;
+import org.axonframework.messaging.unitofwork.UnitOfWork;
+import org.axonframework.modelling.command.AggregateNotFoundException;
+import org.axonframework.modelling.command.Repository;
+import org.springframework.stereotype.Component;
+import java.util.Optional;
+
+@Component
 public class EditArtistValidator {
+    private final ArtistRegistryRepository artistRegistryRepository;
+    private final Repository<Artist> artistRepository;
+    public EditArtistValidator(ArtistRegistryRepository artistRegistryRepository, Repository<Artist> artistRepository) {
+        this.artistRegistryRepository = artistRegistryRepository;
+        this.artistRepository = artistRepository;
+    }
+
+    public Notification validate(EditArtistRequest editArtistRequest){
+        Notification notification = new Notification();
+        String artistId = editArtistRequest.getId().trim();
+        if (artistId.isEmpty()) {
+            notification.addError("Customer id is required");
+        }
+        loadArtistAggregate(artistId);
+
+        String firstName = editArtistRequest.getFirstname().trim();
+        if (firstName.isEmpty()) {
+            notification.addError("Artist firstname is required");
+        }
+        String lastName = editArtistRequest.getLastname().trim();
+        if (lastName.isEmpty()) {
+            notification.addError("Artist lastname is required");
+        }
+        String alias = editArtistRequest.getAlias().trim();
+        if (alias.isEmpty()) {
+            notification.addError("Artist alias is required");
+        }
+        String description = editArtistRequest.getDescription().trim();
+        if (description.isEmpty()) {
+            notification.addError("Artist description is required");
+        }
+        String phrase = editArtistRequest.getPhrase().trim();
+        if (phrase.isEmpty()) {
+            notification.addError("Artist phrase is required");
+        }
+
+        if (notification.hasErrors()) {
+            return notification;
+        }
+
+        return notification;
+    }
+
+    private void loadArtistAggregate(String artistId){
+        UnitOfWork unitOfWork = null;
+        try {
+            unitOfWork = DefaultUnitOfWork.startAndGet(null);
+            artistRepository.load(artistId);
+            unitOfWork.commit();
+        } catch (AggregateNotFoundException ex) {
+            unitOfWork.commit();
+            throw ex;
+        } catch (Exception ex) {
+            if (unitOfWork != null) unitOfWork.rollback();
+        }
+    }
 }
--- ArtistMicroservice/src/main/java/com/example/artistmicroservice/command/application/validators/RegisterArtistValidator.java
+++ ArtistMicroservice/src/main/java/com/example/artistmicroservice/command/application/validators/RegisterArtistValidator.java
@@ -1,10 +1,48 @@
 package com.example.artistmicroservice.command.application.validators;
 
+import com.example.artistmicroservice.command.application.dtos.request.EditArtistRequest;
+import com.example.artistmicroservice.command.application.dtos.request.RegisterArtistRequest;
+import com.example.artistmicroservice.command.infrastructure.ArtistRegistry;
+import com.example.artistmicroservice.command.infrastructure.ArtistRegistryRepository;
+import com.example.artistmicroservice.common.application.Notification;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class RegisterArtistValidator {
-    public RegisterArtistValidator() {}
+    private final ArtistRegistryRepository artistRegistryRepository;
+    public RegisterArtistValidator(ArtistRegistryRepository artistRegistryRepository) {
+        this.artistRegistryRepository = artistRegistryRepository;
+    }
 
+    public Notification validate(RegisterArtistRequest registerArtistRequest) {
+        Notification notification = new Notification();
 
+        String firstName = registerArtistRequest.getFirstname().trim();
+        if (firstName.isEmpty()) {
+            notification.addError("Artist firstname is required");
+        }
+        String lastName = registerArtistRequest.getLastname().trim();
+        if (lastName.isEmpty()) {
+            notification.addError("Artist lastname is required");
+        }
+        String alias = registerArtistRequest.getAlias().trim();
+        if (alias.isEmpty()) {
+            notification.addError("Artist alias is required");
+        }
+        String description = registerArtistRequest.getDescription().trim();
+        if (description.isEmpty()) {
+            notification.addError("Artist description is required");
+        }
+        String phrase = registerArtistRequest.getPhrase().trim();
+        if (phrase.isEmpty()) {
+            notification.addError("Artist phrase is required");
+        }
+
+        if (notification.hasErrors()) {
+            return notification;
+        }
+        return notification;
+    }
 }
